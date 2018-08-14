ca6cd64d5a3c3590551df8e58ad576200248f325
isabellvalente <ivalente@spartaglobal.com>
2018-08-14 20:50:53 +0100

    Initial check-in of module Bingo

---
+++ Bingo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+namespace Bingo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random randNum = new Random();
+            int[] test2 = Enumerable
+                .Repeat(0, 25)
+                .Select(i => randNum.Next(1, 99))
+                .ToArray();
+            foreach (var entry in test2)
+            {
+                Console.WriteLine(entry);
+            }
+        }
+    }
+}
+
